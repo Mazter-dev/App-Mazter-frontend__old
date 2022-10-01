19cb58997fe2208c6d8fe5775f2848cd6f7dd1d5
NelsonDJCR <dev@nelsondjcr.com>
2022-10-01 14:53:04 -0500

    Show products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,18 +3,24 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Master from "./layouts/Master";
 const ProductList = () => {
-	
   // get products
   const [products, setListProducts] = useState([]);
+  const [show, setShow] = useState(false);
+  const [hideFilter, statusHideFilter] = useState(true);
   useEffect(() => {
-    const obtenerPersonajes = async () => {
+    const getProducts = async () => {
       const url = "http://mazter-backend.develop/api/products/get";
       const result = await axios.get(url);
-      console.log(result.data);
       setListProducts(result.data);
     };
-    obtenerPersonajes();
+    getProducts();
   }, []);
+
+  function toggleFilters() {
+    statusHideFilter(false);
+    setShow(!show);
+  }
+
   return (
     <Master>
       <div className="page-wrapper">
@@ -22,17 +28,35 @@ const ProductList = () => {
           <div className="page-header">
             <div className="row">
               <div className="col">
-                <h3 className="page-title">Lista de productos</h3>
+                <h3 className="page-title">
+                  Lista de productos
+                </h3>
               </div>
               <div className="col-auto text-right">
-                <Link className="btn btn-white filter-btn" id="filter_search">
+                <Link
+                  className={`btn btn-white filter-btn ${
+                    show ? "filter-btn-select" : null
+                  }`}
+                  onClick={() => {
+                    toggleFilters();
+                  }}
+                  id="filter_search"
+                >
                   <i className="fas fa-filter"></i>
                 </Link>
               </div>
             </div>
           </div>
 
-          <div className="card filter-card" id="">
+          <div
+            className={`card filter-card 
+			
+			${hideFilter ? "d-none" : "d-block"} 
+			${show ? "show-element" : "hide-element"} 
+			
+			`}
+            id=""
+          >
             <div className="card-body pb-0">
               <form action="#">
                 <div className="row filter-row">
@@ -100,6 +124,78 @@ const ProductList = () => {
             </div>
           </div>
 
+          {/* {show ? (
+            <div className="card filter-card" id="">
+              <div className="card-body pb-0">
+                <form action="#">
+                  <div className="row filter-row">
+                    <div className="col-sm-6 col-md-3">
+                      <div className="form-group">
+                        <label>Provider</label>
+                        <select className="form-control select">
+                          <option>Select Provider</option>
+                          <option>Thomas Herzberg</option>
+                          <option>Matthew Garcia</option>
+                          <option>Yolanda Potter</option>
+                          <option>Ricardo Flemings</option>
+                          <option>Maritza Wasson</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                      <div className="form-group">
+                        <label>Status</label>
+                        <select className="form-control select">
+                          <option>Select Status</option>
+                          <option>Pending</option>
+                          <option>Inprogress</option>
+                          <option>Completed (Provider)</option>
+                          <option>Accepted</option>
+                          <option>Rejected</option>
+                          <option>Cancelled (Provider)</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                      <div className="form-group">
+                        <label>From Date</label>
+                        <div className="cal-icon">
+                          <input
+                            className="form-control datetimepicker"
+                            type="text"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                      <div className="form-group">
+                        <label>To Date</label>
+                        <div className="cal-icon">
+                          <input
+                            className="form-control datetimepicker"
+                            type="text"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                      <div className="form-group">
+                        <button
+                          className="btn btn-primary btn-block"
+                          type="submit"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          ) : (
+            <div style={{ color: "blue" }}>Div 2</div>
+          )} */}
+
           <div className="row">
             <div className="col-md-12">
               <div className="card">
@@ -126,8 +222,8 @@ const ProductList = () => {
                               <td>{i.price}</td>
                               <td>{i.stock}</td>
                               <td>{i.sales}</td>
-                              <td>{i.state === 1 ? 'active' :'inactive'} </td>
-                        
+                              <td>{i.state === 1 ? "active" : "inactive"} </td>
+
                               <td>
                                 <Link
                                   href="edit-ratingstype.html"
