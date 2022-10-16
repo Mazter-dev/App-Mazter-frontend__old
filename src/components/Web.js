76272b6fd494d9e8fb024d2b846a4b147b02890a
NelsonDJCR <dev@nelsondjcr.com>
2022-10-16 12:01:15 -0500

    Changes

--- src/components/Web.js
+++ src/components/Web.js
@@ -1,44 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Api from "./test/Api";
-import CashRegister from "./CashRegister";
-import Dashboard from "./Dashboard";
-import MyData from "./MyData";
-import MySuscription from "./MySuscription";
-import ProductAdd from "./ProductAdd";
-import ProductList from "./ProductList";
-import Error404 from "./errors/Error404";
-import Login from "./Login";
-import Test from "./test/Test";
+// import {  Route } from "react-router-dom";
+// import Api from "./inventory/test/Api";
+// import CashRegister from "./inventory/CashRegister";
+// import Dashboard from "./inventory/Dashboard";
+// import MyData from "./inventory/MyData";
+// import MySuscription from "./inventory/MySuscription";
+// import ProductAdd from "./inventory/ProductAdd";
+// import ProductList from "./inventory/ProductList";
+// import Error404 from "./errors/Error404";
+// import Login from "./inventory/Login";
+// import Test from "./inventory/test/Test";
 const Web = () => {
-    // Valide login
-
     return (
-        // <switch>
-        //     <Route path="/" render={()=>{
-
-        //         <Login />
-        //     }} >
-        //     </Route>
-        // </switch>
-
-        <Routes>
-            {/* <Route path="/" element={<Xd />} /> */}
-            <Route path="/Dashboard" element={<Dashboard />} />
-
-            <Route path="/CashRegister" element={<CashRegister />} />
-            <Route path="/Add-product" element={<ProductAdd />} />
-            <Route path="/List-product" element={<ProductList />} />
-            <Route path="/My-data" element={<MyData />} />
-            <Route path="/My-suscription" element={<MySuscription />} />
-            <Route path="/test-api" element={<Api />} />
-            <Route path="/test" element={<Test />} />
-            <Route path="/test-login" element={<Login />} />
-            <Route path="*" element={<Error404 />} />
-            {/* <Route path="*">
-              404
-            </Route> */}
-        </Routes>
+        <div>
+           
+        </div>
     );
 };
 
