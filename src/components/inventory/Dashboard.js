7e7c110386dc89b14491447c073c5a5987c5544b
NelsonDJCR <dev@nelsondjcr.com>
2022-11-07 10:56:04 -0500

    Delete import jquery

--- src/components/inventory/Dashboard.js
+++ src/components/inventory/Dashboard.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Master from "./layouts/Master";
-import $ from 'jquery'
 
 const Dashboard = () => {
     const navigate = useNavigate();
