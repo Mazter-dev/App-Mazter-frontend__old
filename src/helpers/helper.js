f20d2343e1c2fb48321307313237693f383ae0aa
NelsonDJCR <dev@nelsondjcr.com>
2022-12-15 18:46:17 -0500

    Update connection api

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -2,7 +2,7 @@ export function getBearer() {
     return localStorage.getItem("bearer");
 }
 export function urlApi(url) {
-    return "http://mazter-backend.develop/api/v1/" + url;
+    return "https://api.nelsondjcr.com/api/v1/" + url;
     // return 'https://api.nelsondjcr.com/api/v1/'+ url;
 }
 export function configApi() {
