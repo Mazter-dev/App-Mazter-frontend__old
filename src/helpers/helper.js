74cf93bdf73649c76ab943e4e73071caed9ab61b
NelsonDJCR <dev@nelsondjcr.com>
2023-02-05 22:57:00 -0500

    test

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -3,6 +3,7 @@ export function getBearer() {
 }
 export function urlApi(url) {
     return "http://mazter-backend.develop/api/v1/" + url;
+    //  return "http://mazter-backend.develop/api/v1/" + url;
     // return 'https://api.nelsondjcr.com/api/v1/'+ url;
 }
 export function configApi() {
